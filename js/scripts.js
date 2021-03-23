d1e2214b9d148c313310d975cf3628c77b2784f4
basicjohn <edmondsonj@gmail.com>
2021-03-23 14:58:05 -0700

    connect show card script to each li
    co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,16 @@ $(document).ready(function () {
   $("li#string h2").click(function () {
     $("li#string .card").toggle();
   });
+  $("li#boolean h2").click(function () {
+    $("li#boolean .card").toggle();
+  });
+  $("li#null h2").click(function () {
+    $("li#null .card").toggle();
+  });
+  $("li#undefined h2").click(function () {
+    $("li#undefined .card").toggle();
+  });
+  $("#symbol h2").click(function () {
+    $("#symbol .card").toggle();
+  });
 });
